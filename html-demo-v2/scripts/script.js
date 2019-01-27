9b09d8a3b12634d40bfad176efce1575c218e5d3
LYU.Shulun <lyu@192.168.1.3>
2019-01-27 16:27:56 +0800

    updated

--- html-demo-v2/scripts/script.js
+++ html-demo-v2/scripts/script.js
@@ -6,8 +6,8 @@ info.textContent = 'Below is a dynamic list. Click anywhere outside the list to'
  + 'add a new list item. Click an existing list item to change its text to'
  + 'something else.';
 
- document.body.appendChild(info);
- document.body.appendChild(list);
+document.body.appendChild(info);
+document.body.appendChild(list);
 
  html.onclick = function() {
      var listItem = document.createElement('li');
@@ -15,7 +15,7 @@ info.textContent = 'Below is a dynamic list. Click anywhere outside the list to'
      listItem.textContent = listContent;
      list.appendChild(listItem);
 
-     listItem.onclick(e) {
+     listItem.onclick = function(e) {
          e.stopPropagation();
          var listContent = prompt('Enter new content for your list item:');
          this.textContent = listContent;
